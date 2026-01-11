68c6da30763d3c209467eb0051c44d7f7d31d4c0
SoftCurrent-agency <SoftCurrentagency@gmail.com>
2026-01-11 14:52:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   base: "/Soft-Current-/",
-  define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-  },
   build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
+    outDir: "dist",
+    sourcemap: false,
+  },
 });
-
-
